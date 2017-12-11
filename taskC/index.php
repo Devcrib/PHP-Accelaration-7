a1849f97c3a51b9d784b66739d26048fafefec99
Falua Temitope Oyewole <emmat0616@gmail.com>
2017-12-11 19:10:26 +0100

    c

--- taskC/index.php
+++ taskC/index.php
@@ -22,9 +22,23 @@ class StudentResult {
 
     public function getScore() {
         for($i = 0; $i < $this->totalStudents; $i++) {
-//            array_push($this->score, $this->student[$i]->score);
-            echo $this->grade($this->student[$i]->score);
+            $this->score[ $this->student[$i]->name] = $this->grade($this->student[$i]->score);
         }
+        asort($this->score);
+    }
+
+    /**
+     * @param $id
+     * @return stdClass
+     */
+    public function studentDetail($id) {
+        $student = new stdClass;
+        $newprop = 'grade';
+        foreach ($this->student[$id] as $key => $value){
+            $student->$key = $value;
+            $student->$newprop = $this->grade($this->student[$id]->score);
+        }
+        return $student;
     }
 
     /**
@@ -32,17 +46,35 @@ class StudentResult {
      * @return bool|string
      */
     public function grade($score) {
-        $grade = ($score > 0 && $score < 40) ?? 'F';
-        $grade = ($score) ? ($score > 39 && $score < 45): 'E';
-        $grade = ($score) ? ($score > 44 && $score < 50): 'D';
-        $grade = ($score) ? ($score > 49 && $score < 60): 'C';
-        $grade = ($score) ? ($score > 59 && $score < 70): 'B';
-        $grade = ($score) ? ($score >= 70  && $score <= 100): 'A';
-        return $grade;
+        if($score >= 0 && $score < 40) return 'F';
+        if($score > 39 && $score < 45) return 'E';
+        if($score > 44 && $score < 50) return'D';
+        if($score > 49 && $score < 60)return 'C';
+        if($score > 59 && $score < 70)return 'B';
+        if($score >= 70  && $score <= 100)return 'A';
+    }
+
+    /**
+     * @return mixed
+     */
+    public function highest_grade() {
+        $max = ['A' => 0, 'B' => 0, 'C' => 0, 'D' => 0, 'E' => 0, 'F' => 0 ];
+        foreach ($this->score as $grade) {
+            if ($grade == 'A') $max['A'] += 1;
+            if ($grade == 'B') $max['B'] += 1;
+            if ($grade == 'C') $max['C'] += 1;
+            if ($grade == 'D') $max['D'] += 1;
+            if ($grade == 'E') $max['E'] += 1;
+            if ($grade == 'F') $max['F'] += 1;
+        }
+        return max($max);
     }
 }
 
 $studentData = new StudentResult('data.json');
 
 $studentData->getScore();
-var_dump($studentData->score);
+var_dump($studentData->highest_grade());
+
+ //print_r($studentData->studentDetail(50));
+//var_dump($studentData->score);
